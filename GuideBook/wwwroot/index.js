437dc6d803379e1fdb7499168f1e98889052ad7d
MakeIt-Mike <mike.isaac.jones@gmail.com>
2021-03-20 18:09:18 -0500

    Adding JS usability. adding in Layout, Coding, and Naming conventions.
    Setting up basic CSS for site,

---
+++ GuideBook/wwwroot/index.js
@@ -0,0 +1,8 @@
+﻿function showAbout() {
+    var message =  "This is a Website Template built by MakeIt-Tech. \r\n" +
+        "This Blazor Project is Open Source. \r\n" +
+        "I only ask that you do not push ANY corporate-owned code to my Git Repo.\r\n" +
+        " Happy Coding :) \r\n" +
+        "https://github.com/makeit-mike/GuideBook"
+    alert(message);
+}
