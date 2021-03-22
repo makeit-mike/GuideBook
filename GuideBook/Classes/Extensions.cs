15c9174a590ab23c307ed2f304763f1447d8fc59
MakeIt-Mike <mike.isaac.jones@gmail.com>
2021-03-22 12:34:29 -0500

    Can make API connection. Able to parse JSON. Reorganized code layout. Added more conventions, snippets, styling, and explanations

---
+++ GuideBook/Classes/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GuideBook.Classes
+{
+    public static class Extensions
+    {
+        public static string ToProperNoun(this string noun)
+        {
+            try
+            {
+                return (noun.Length > 1) ? $"{noun[0].ToString().ToUpper()}{noun[1..]}" : noun.ToUpper();
+            }
+            catch
+            {
+                return noun;
+            }
+        }
+    }
+}
